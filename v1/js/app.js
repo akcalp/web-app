6d5d2b0e82970965eb97ad9fec4f6d745dbc9705
akcalp <alp92akca@gmail.com>
2019-07-09 01:23:53 -0400

    add slider, about, skills

---
+++ v1/js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $('#slides').superslides({
+        animation: "fade",
+        play: 4000,
+        pagination: false
+    });
+
+    const typed = new Typed(".typed", {
+        strings: ["Software Developer.", "Web Developer.", "Student...."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+
+            938: {
+                items: 4
+            }
+        }
+    });
+
+    const skillsTopOffSet = $(".skillsSection").offset().top;
+    $(window).scroll(function () {
+        if (window.pageYOffset > skillsTopOffSet - $(window).height() + 200) {
+            $('.chart').easyPieChart({
+                easing: "easeInOut",
+                barColor: "#fff",
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function (from, to, percent) {
+                    $(this.el).find(".percent").text(Math.round(percent));
+                }
+            });
+        }
+    })
+});
+
